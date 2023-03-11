aecf2047db71e835ad5c35d53722c4c381a69078
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-03-11 15:53:00 -0300

    feat: Adicionado classe "Player"

---
+++ src/Player.java
@@ -0,0 +1,8 @@
+public class Player {
+//    Classe que irá simular um ‘player’ de mídia
+//    Esta classe terá apenas o método que mostrará que algo está sendo reproduzido.
+
+    public static void reproduzir (String conteudo) {
+        System.out.println("Reproduzindo: " + conteudo);
+    }
+}
